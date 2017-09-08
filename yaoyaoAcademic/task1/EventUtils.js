368dccba1c4440ae80aa33fd7ff0f47499f8a7e1
whuzxq <1092621818@qq.com>
2017-09-08 21:31:26 +0800

    commit

---
+++ yaoyaoAcademic/task1/EventUtils.js
@@ -0,0 +1,26 @@
+/*
+* @Author: whuzxq
+* @Date:   2017-08-31 09:27:22
+* @Last Modified by:   whuzxq
+* @Last Modified time: 2017-08-31 09:43:18
+*/
+var EventUtil={
+	addHandler:function(element,type,handler){
+		if(element.addEventListener){
+			element.addEventListener(type,handler,false);
+		}else if(element.attachEvent){
+			element.attachEvent("on"+type,handler);
+		}else{
+			element["on"+type]=handler;
+		}
+	},
+	removeHandler:function(element,type,handler){
+		if(element.removeEventListener){
+			element.removeEventListener(type,handler,false);
+		}else if(element.detachEvent){
+			element.detachEvent("on"+type,handler);
+		}else{
+			element["on"+type]=null;
+		}
+	}
+};
